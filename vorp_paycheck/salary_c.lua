5ed0aa48a1e2eec5d4db1a95e1aec5aefe0e1605
Artzalez <devbiitz2@gmail.com>
2020-10-25 15:07:23 +0000

    2.0 Convert

--- vorp_paycheck/salary_c.lua
+++ vorp_paycheck/salary_c.lua
@@ -1,9 +1,15 @@
 function a() return 'b' end
 local minutes = 15
 --> Minutos to give. By default is 15
-Citizen.CreateThread(function(...)
-	while true do
-	Citizen.Wait(minutes*60000)
-		TriggerServerEvent('salaryJobs:GIVE',"0x089027928098908_", a());
-	end
+
+RegisterNetEvent("vorp:SelectedCharacter")
+AddEventHandler("vorp:SelectedCharacter", function(charid)
+    -- Code here will be executed once the event is triggered.
+    -- Like a getcharacter or load db data, etc ....
+	Citizen.CreateThread(function(...)
+		while true do
+		Citizen.Wait(minutes*60000)
+			TriggerServerEvent('salaryJobs:GIVE',"0x089027928098908_", a());
+		end
+	end)
 end)
